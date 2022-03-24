624f74fa311ae212b262b7daf90e946930cd48c4
SrCaronte <crismonothian@gmail.com>
2022-03-24 11:02:22 -0500

    homework1al6

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,13 +4,13 @@
 const nuevaString = "lol";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = 20;
 
 // Crea una variable booleana:
-const nuevoBool = null;
+const nuevoBool = cristhian = true;
 
 // Resuelve el siguiente problema matemático:
-const nuevaResta = 10 - null === 5;
+const nuevaResta = 10 - 5 === 5;
 
 // Resuelve el siguiente problema matemático:
 const nuevaMultiplicacion = 10 * null === 40 ;
